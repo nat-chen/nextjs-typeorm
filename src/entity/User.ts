6a6637ef6deced24e26e3ab1d886a67ca04b667d
Nat Chen <chen_natalie@outlook.com>
2021-06-15 22:44:46 +0800

    创建三个 entity

---
+++ src/entity/User.ts
@@ -0,0 +1,21 @@
+ import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import { Post } from "./Post";
+import { Comment } from './Comment';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column('varchar')
+  username: string;
+  @Column('varchar')
+  passwordDigest: string;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  udpatedAt: Date;
+  @OneToMany(type => Post, post => post.author)
+  posts: Post[];
+  @OneToMany(type => Comment, comment => comment.user)
+  comments: Comment[];
+}
