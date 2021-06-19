38c99acc10f4999b687306bfe2746bedb38f9d20
Nat Chen <chen_natalie@outlook.com>
2021-06-19 18:41:46 +0800

    展示 posts

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,17 +1,23 @@
 import { createConnection, getConnectionManager } from "typeorm";
+import 'reflect-metadata';
+import { Post } from 'src/entity/Post';
+import { User } from 'src/entity/User';
+import { Comment } from 'src/entity/Comment';
+import config from 'ormconfig.json';
+
+const create = async() => {
+  // @ts-ignore
+  return createConnection({
+    ...config,
+    entities: [Post, User, Comment]
+  })
+}
 
 const promise = (async function() {
   const manager = getConnectionManager();
-  if (!manager.has('default')) {
-    return createConnection();
-  } else {
-    const current = manager.get('default');
-    if (current.isConnected) {
-      return current;
-    } else {
-      return createConnection();
-    }
-  }
+  const current = manager.has('default') && manager.get('default');
+  if (current) { await current.close() };
+  return create();
 })();
 
 export const getDatabaseConnection = async() => {
