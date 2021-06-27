3cbb7a0d2cd141177cea3e245bff2d292eece657
Nat Chen <chen_natalie@outlook.com>
2021-06-27 08:36:20 +0800

    优化小细节

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,23 +1,16 @@
-import { NextApiHandler } from 'next';
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 import * as next from 'next';
-declare module "*.png" {
+
+declare module '*.png' {
   const value: string;
   export default value;
 }
 
-type Post = {
-  id: string;
-  date: string;
-  title: string;
-  content: string;
-  htmlContent: string;
-}
-
 declare module 'next' {
   import {Session} from 'next-iron-session';
-  interface NextApiHandler {
+
+  interface NextApiRequest {
     session: Session
   }
 }
